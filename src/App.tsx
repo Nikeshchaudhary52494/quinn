538f5a4aff9d5d7c4dd4094bef83b0fd8ba106a7
nikesh chaudhary <nikeshchaudhary52494@gmail.com>
2025-09-01 20:00:43 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex justify-center items-center text-5xl bg-black text-white h-screen uppercase font-bold">
+      Quinn
+    </div>
+  );
+}
+
+export default App;
